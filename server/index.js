32748fd8e1d7da8c2b6fcdb33e7034b43ac474d7
knox26 <bvjadhav7558@gmail.com>
2024-12-27 13:22:38 +0530

    modified:   Client/.env 	modified:   server/.env 	modified:   server/index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,6 @@ const dbUrl = process.env.DATABASE_URL;
 //cors setup
 app.use(
   cors({
-    origin: process.env.ORIGIN,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
