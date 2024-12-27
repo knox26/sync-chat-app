863ca2a936553344a4ba5ec4b7b2a845dcedcf0a
knox26 <bvjadhav7558@gmail.com>
2024-12-27 15:09:47 +0530

    modified:   Client/.env 	modified:   server/middleware/AuthMiddleware.js 	modified:   Client/.env 	modified:   server/controllers/AuthController.js

--- server/middleware/AuthMiddleware.js
+++ server/middleware/AuthMiddleware.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.jwt;
+  console.log("token from verify token", token);
 
   if (!token) {
     return res.status(401).send("Unauthorized");
